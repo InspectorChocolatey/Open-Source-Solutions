24b02a612be7beb682f50ce9c6d7a8377ee736c4
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2018-03-03 02:20:46 +0000

    Update dotnetcore.sh

--- NewHorizons/dotnetcore.sh
+++ NewHorizons/dotnetcore.sh
@@ -1,6 +1,12 @@
 ###########################
  #<New Linux Script Tech>#
 ###########################
+#
+# References:
+#
+#  https://www.youtube.com/watch?v=VzaYGMi5LvY
+#
+#########################################################
 
 # Prevent invalid signature errors when trying to update the system:
 wget -q -O - https://archive.kali.org/archive-key.asc  | apt-key add
