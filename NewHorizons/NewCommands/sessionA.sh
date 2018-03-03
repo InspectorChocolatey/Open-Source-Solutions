c3bed2bd07a42f499a7db0d02b3527f312f8deb3
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2018-03-03 23:08:57 +0000

    Update sessionA.sh

--- NewHorizons/NewCommands/sessionA.sh
+++ NewHorizons/NewCommands/sessionA.sh
@@ -17,3 +17,10 @@ netstat -tulpn;
 
 # Equivalent to Net.WebClient.DownloadString("$url");
 curl localhost;
+
+
+# Who or What is currently using something?
+# for a directory:
+lsof "/opt/sublime_text"; 
+# for a program:
+lsof "/opt/sublime_text/sublime_text";
