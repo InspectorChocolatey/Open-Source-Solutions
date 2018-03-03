5d5753703acabeadf9523211dc5c6229e3440e54
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2018-03-03 02:37:25 +0000

    Update dotnetcore.sh

--- NewHorizons/dotnetcore.sh
+++ NewHorizons/dotnetcore.sh
@@ -1,12 +1,12 @@
 ###########################
  #<New Linux Script Tech>#
 ###########################
-#
 # References:
 #
-#  https://www.youtube.com/watch?v=VzaYGMi5LvY
+#	https://www.youtube.com/watch?v=VzaYGMi5LvY
 #
-#########################################################
+########################################################
+
 
 # Prevent invalid signature errors when trying to update the system:
 wget -q -O - https://archive.kali.org/archive-key.asc  | apt-key add
@@ -15,3 +15,20 @@ apt-get update -y;
 
 apt-get install wine -y;
 apt-get install winetricks wine32 -y;
+
+##############################
+ #<Install dotnet framework>#
+##############################
+
+# for 64-bit:
+
+wget http://www.kegel.com/wine/winetricks
+
+chmod +x winetricks
+
+mv winetricks /usr/bin
+
+WINEPREFIX=~/.wine32 WINEARCH=win32 wineboot
+WINEPREFIX=~/.wine32 WINEARCH=win32 winetricks dotnet45
+WINEPREFIX=~/.wine32 WINEARCH=win32 winetricks dotnet20
+
