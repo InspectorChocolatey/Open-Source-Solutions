96530b79a1085df536baa47ab7aed2820ba1b40f
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2018-03-08 20:58:36 -0800

    Update customizeterminal.sh

--- customizeterminal.sh
+++ customizeterminal.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+# references:
+#   http://osxdaily.com/2006/12/11/how-to-customize-your-terminal-prompt/
 
-# Rename the host:pwd
+# rename the host:pwd
 hostname "ProximaCentauri";
 
 # edit your .bashrc file:
 nano "/$USER/.bashrc";
 
-# add the following to the bottom of 
-# your profile:
+# add the following to the bottom of your profile:
 export PS1="\W @ \h [\u] $ "
