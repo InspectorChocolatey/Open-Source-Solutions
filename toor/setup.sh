906988ccb83fa87cc2438cabc7b53e6eaa0d3db1
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2018-03-18 04:14:01 +0000

    Update setup.sh

--- toor/setup.sh
+++ toor/setup.sh
@@ -2,6 +2,10 @@
 ########################################
  # <toor browser installation package>#
 ########################################
+#
+# references:
+# https://www.youtube.com/watch?v=o9lyGUyJr7A
+#
 # apt-get update error: invalid signature:
 #https://unix.stackexchange.com/questions/421985/invalid-signature-when-trying-to-apt-get-update-on-kali
 wget -q -O - https://archive.kali.org/archive-key.asc  | apt-key add
@@ -16,3 +20,7 @@ firefox "$url";
 cd "/$USER/Downloads";
 file="tor-browser-linux64-7.5.2_en-US.tar.xz";
 tar -xf "$file";
+
+# make it so that you can run toor browser as root (as seen in the video:)
+cd "/root/Downloads/tor-browser_en-US"
+./start-toor-browser
