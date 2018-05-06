c36a3c49b1da6dddf3111ca82aa7ef6dd3aceed5
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2018-05-06 02:34:06 +0000

    Create webdevsetup.sh

---
+++ webdevsetup.sh
@@ -0,0 +1,30 @@
+############################
+ #<Open Source Solutions>#
+############################
+
+[] sublime
+[] codeblocks
+[] php stack....
+
+java wamp server installation
+
+[] a JavaBased GUI
+
+[ ] implement the credentialcaching
+[ ] xmlbased packagemanager
+[ ] git clone command can be ripped thru for the interfaces specified in XML...
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+root="/$USER/Desktop";
+cd "$root";
+dir="OpenSourceWeb";
+mkdir "$dir" -v;
+cd "$dir";
+url="https://github.com/InspectorChocolatey/Xml";
+git clone "$url";
+
+
+url="https://github.com/InspectorChocolatey/Java";
+git clone "$url";
+
