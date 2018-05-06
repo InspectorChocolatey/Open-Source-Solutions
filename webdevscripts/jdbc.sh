39096b86f7c6b3f8b4665db6f10f11c08dd77ade
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2018-05-06 16:49:29 +0000

    Create jdbc.sh

---
+++ webdevscripts/jdbc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+## Download Microsoft JDBC Driver for SQL Server
+# https://docs.microsoft.com/en-us/sql/connect/jdbc/download-microsoft-jdbc-driver-for-sql-server?view=sql-server-2017
