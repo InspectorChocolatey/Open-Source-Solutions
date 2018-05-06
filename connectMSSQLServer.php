ab8f3f39eb88b0df6cfd2c052e526db3fea874bf
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2018-05-06 04:07:44 +0000

    Create connectMSSQLServer.php

---
+++ connectMSSQLServer.php
@@ -0,0 +1,19 @@
+<!--
+	references:
+	http://php.net/manual/en/function.mssql-connect.php
+-->
+
+<?php
+// Server in the this format: <computer>\<instance name> or 
+// <server>,<port> when using a non default port number
+$server = 'KALLESPC\SQLEXPRESS';
+
+// Connect to MSSQL
+$link = mssql_connect($server, 'sa', 'dbs');
+
+if (!$link) 
+{
+    die('Something went wrong while connecting to MSSQL');
+}
+
+?>
